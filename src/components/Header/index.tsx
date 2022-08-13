5cb406b1dc6f58e06ba92805f7098cd278768b2f
McLovin <aurinhorodrigues@gmail.com>
2022-08-13 10:52:46 -0300

    finalizando a responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import './index.css'
 import { FaRegBell, FaBell } from 'react-icons/fa'
 import { useState } from 'react';
-import CoinApi from '../../services/CoinApi';
 
 function Header() {
     const [activeBell, setActiveBell] = useState<boolean>(false);
+
 
     return (
         <>
             <header id="header">
                 <div id="header-container-img-logo">
                     <img id="img-logo" src="/images/logo.png" alt="Imagem do site Kahuna"/>
-                    <p id="texto-logo">ahuna</p>
+                    <p id="text-logo">ahuna</p>
                 </div>
                 <div id="header-container-btn">
                     <button className="header-btn" id="btn-add-invest">Gerenciar</button>
@@ -32,7 +32,7 @@ function Header() {
                 </div>
             </header>
         </>
-     );
+    );
 }
 
 export default Header;
