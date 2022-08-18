c4310b2ce178e3d8417639953eb2da1595b6ee6b
eduardodsn <carlosedsb0110@gmail.com>
2022-08-18 17:31:55 -0300

    Adiciona prop homepage

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
                 position="top-center"
                 reverseOrder={false}
             />
-            <Router>
+            <Router basename='/'>
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/manager' element={<Manager/>} />
